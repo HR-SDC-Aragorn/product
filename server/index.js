95bb15031867dec2a60848c1ad4ccac4b71ea0b5
Siming Yum <103225149+simingyum@users.noreply.github.com>
2022-09-21 16:44:40 -0700

    add loderio script

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/', router);
 
+app.get("/loaderio-bedd23ec0b5c145c0e72b8ee942c3d6b", (req, res) => res.send("loaderio-bedd23ec0b5c145c0e72b8ee942c3d6b"))
+
 app.listen(process.env.PORT, (err)=> {
   if (err) {
     console.log(err);
