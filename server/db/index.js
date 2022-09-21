996408b6fd3837b5dc5078db90fe50233c97417a
Siming Yum <103225149+simingyum@users.noreply.github.com>
2022-09-21 15:27:27 -0700

    update database info

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,19 @@
 const { Pool } = require('pg');
 
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT
+// });
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
 });
+
 
 module.exports = pool;
